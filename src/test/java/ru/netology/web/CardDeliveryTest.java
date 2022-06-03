4753a80f6706bc226809a5ea9a917d97992526ce
Александр Сиденов <sid_san@mail.ru>
2022-06-03 13:08:44 +0800

    add Test

---
+++ src/test/java/ru/netology/web/CardDeliveryTest.java
@@ -0,0 +1,27 @@
+package ru.netology.web;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selectors.*;
+import java.time.Duration;
+
+public class CardDeliveryTest {
+
+    @Test
+    void shouldRegisterByAccount() {
+        Configuration.holdBrowserOpen = true;
+        open("http://localhost:9999");
+        $("[placeholder=\"Дата встречи\"]").setValue("06.06.2022");
+        $("[placeholder=\"Город\"]").setValue("Москва");
+
+        $("[data-test-id=\"name\"] input").setValue("Иванов Иван");
+        $("[data-test-id=\"phone\"] input").setValue("+79991234567");
+        $("[data-test-id=\"agreement\"]").click();
+        $x("//*[text()=\"Забронировать\"]").click();
+        $(byText("Встреча успешно забронирована на")).should(visible,Duration.ofSeconds(15));
+
+    }
+}
