165e89f0eac58401f0e2bfa5c5ad5888cf5037b7
Александр Сиденов <sid_san@mail.ru>
2022-06-04 16:33:17 +0800

    add planindate

--- src/test/java/ru/netology/web/CardDeliveryTest.java
+++ src/test/java/ru/netology/web/CardDeliveryTest.java
@@ -4,23 +4,32 @@ import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Selenide.*;
-import static com.codeborne.selenide.Condition.*;
-import static com.codeborne.selenide.Selectors.*;
 import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import org.openqa.selenium.Keys;
+import com.codeborne.selenide.Condition;
 
 public class CardDeliveryTest {
 
+    public String generateDate(int days) {
+        return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+    }
+
     @Test
     void shouldRegisterByAccount() {
+        String planningDate = generateDate(4);
         Configuration.holdBrowserOpen = true;
         open("http://localhost:9999");
 
         $("[placeholder=\"Город\"]").setValue("Москва");
-        $("[placeholder=\"Дата встречи\"]").setValue("06.06.2022");
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        $("[placeholder=\"Дата встречи\"]").setValue(planningDate);
         $("[data-test-id=\"name\"] input").setValue("Иванов Иван");
         $("[data-test-id=\"phone\"] input").setValue("+79991234567");
         $("[data-test-id=\"agreement\"]").click();
         $x("//*[text()=\"Забронировать\"]").click();
-        $(byText("Встреча успешно забронирована на")).should(visible,Duration.ofSeconds(15));
+        $(".notification__content")
+                .shouldHave(Condition.text("Встреча успешно забронирована на " + planningDate), Duration.ofSeconds(15));
     }
 }
